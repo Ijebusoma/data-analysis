d71f1af9e0e813eb87baa90be3856792e8c61e1c
Ijebusoma <vanessaosuka@gmail.com>
2017-11-14 23:13:34 +0100

    adjusted code to conform to pep8 standard

--- newsdata.py
+++ newsdata.py
@@ -3,7 +3,7 @@ import psycopg2
 DBNAME = "news"
 
 
-def execute_query(query): #function to execute all the queries
+def execute_query(query):
     try:
         db = psycopg2.connect(database=DBNAME)
         c = db.cursor()
@@ -11,7 +11,7 @@ def execute_query(query): #function to execute all the queries
         results = c.fetchall()
         db.close()
         return results
-    except BaseException: #in case something goes wrong
+    except BaseException:
         print("Error while perfoming database operation")
 
 
@@ -20,9 +20,9 @@ def get_articles():
                  log where log.path like concat('%', articles.slug)
                  group by articles.title, articles.author
                  order by views desc limit 3;"""
-    articles = execute_query(query) #executes the popular article query
-    for article in articles: #loop through the results fetched from the database
-        print ("Article name:{}, views:{}".format(article[0], article[1])) #print output
+    articles = execute_query(query)
+    for article in articles:
+        print ("Article name:{}, views:{}".format(article[0], article[1]))
 
 
 def get_authors():
@@ -33,7 +33,7 @@ def get_authors():
             order by views desc;"""
     authors = execute_query(query)
     for author in authors:
-        print("{}".format(author[0])) #loop through and print out author names
+        print("{}".format(author[0]))
 
 
 def get_request():
@@ -49,5 +49,3 @@ print("-----------Most Popular Authors--------------")
 get_authors()
 print("----------Day with much error rate----------")
 get_request()
-
-
